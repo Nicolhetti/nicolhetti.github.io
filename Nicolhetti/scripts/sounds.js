bac4fc7dd7cf0ceafc1c994c395d1c97eaa7e122
nicolasimanolmedina <nicolasimanolmedina@gmail.com>
2025-02-21 05:00:28 +0000

    Add click interaction to enable audio, exclude links

--- Nicolhetti/scripts/sounds.js
+++ Nicolhetti/scripts/sounds.js
@@ -3,14 +3,29 @@ const clickSound = new Audio("sounds/click.mp3");
 
 const buttons = document.querySelectorAll(".button");
 
-buttons.forEach(button => {
-    button.addEventListener("mouseenter", () => {
-        hoverSound.currentTime = 0;
-        hoverSound.play();
-    });
+buttons.forEach((button) => {
+  button.addEventListener("mouseenter", () => {
+    hoverSound.currentTime = 0;
+    hoverSound.play();
+  });
 
-    button.addEventListener("click", () => {
-        clickSound.currentTime = 0;
-        clickSound.play();
-    });
+  button.addEventListener("click", () => {
+    clickSound.currentTime = 0;
+    clickSound.play();
+  });
+});
+
+// Video Play
+document.addEventListener("click", function (event) {
+  let video = document.getElementById("bgVideo");
+  let audioMessage = document.getElementById("audioMessage");
+
+  if (event.target.tagName.toLowerCase() === "a") {
+    return;
+  }
+
+  if (video.muted) {
+    video.muted = false;
+    audioMessage.classList.add("hidden");
+  }
 });
